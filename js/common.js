a89e96611d67de5aa369d7a479ab786df22ab46d
nt-0123 <nt_onl_ysin.wb@docomo.ne.jp>
2015-07-10 16:59:12 +0900

    追加

--- js/common.js
+++ js/common.js
@@ -1,17 +1,8 @@
 
-$('body').random([
-	'<img src="img/backgroundImg/thumb_1.png">',
-	'<img src="img/backgroundImg/thumb_2.png">',
-	'<img src="img/backgroundImg/thumb_3.png">',
-	'<img src="img/backgroundImg/thumb_4.png">',
-	'<img src="img/backgroundImg/thumb_5.png">',
-	'<img src="img/backgroundImg/thumb_6.png">',
-	'<img src="img/backgroundImg/thumb_10.png">',
-	'<img src="img/backgroundImg/thumb_11.png">',
-	'<img src="img/backgroundImg/thumb_12.png">',
-	'<img src="img/backgroundImg/thumb_13.png">',
-	'<img src="img/backgroundImg/thumb_14.png">',
-	'<img src="img/backgroundImg/thumb_16.png">',
-],{
-num: 200
-});
+$(".colorbox").colorbox({iframe:true, width:"80%", height:"80%"});
+
+
+$('.tab').dataExtend('tab');
+
+
+$('.toggle').dataExtend('extraToggleClass');

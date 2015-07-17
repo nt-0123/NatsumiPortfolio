4fcc59269833e1de6419b487358d6d54339f6552
kamem <develo0@gmail.com>
2015-07-17 14:48:15 +0900

    コンテンツにparallaxの効果をつける

--- js/common.js
+++ js/common.js
@@ -4,8 +4,38 @@ if($colorbox.size() > 0) {
 	$colorbox.colorbox({iframe:true, width:"80%", height:"80%"});
 }
 
-
 $('.tab').dataExtend('tab');
 
-
 $('.toggle').dataExtend('extraToggleClass');
+
+
+var $content = $('#header, #about, #work, #contact');
+
+//scroll
+$content.each(function(i){
+	var top = parseInt($(this).offset().top);
+
+	$('[href="#' + $(this).prop('id') + '"]').on('click', function() {
+		console.log(top);
+		$('html,body').animate({scrollTop: top}, 2000, 'easeOutQuart');
+	});
+});
+
+//parallax
+$content.each(function(i){
+	var top = parseInt($(this).offset().top);
+	var height = parseInt($(this).height());
+	var init = -(top + height);
+	$(this).css({
+		top: init
+	});
+	$(window).parallax({
+		parallax : $(this),
+		type : 'scrollSpeed',
+		style : 'top',
+		fixPosition : top,
+		speed : 0.5,
+		adjustment : top
+	});
+});
+
